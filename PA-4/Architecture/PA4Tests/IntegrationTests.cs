53a7afdc0c002bd0968710d51aec5b1bc9d47ddd
Krishane Riegodedios <krishane@WIN2.CS.KSU.EDU>
2018-05-06 18:42:04 -0500

    Integration Test!!!

---
+++ PA-4/Architecture/PA4Tests/IntegrationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PA_4_GUI;
+
+namespace PA4Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void TestAllTestDB()
+        {
+            Product p = new Product("Test", 1, 1.99);
+
+            Tuple<Product, int> tuple = new Tuple<Product, int>(p, 1);
+
+            List<Tuple<Product, int>> l = new List<Tuple<Product, int>>();
+
+            l.Add(tuple);
+
+            Transaction trans = new Transaction(1, l);
+
+            TransactionDB.AddTransaction(trans);
+
+            Assert.Equals(TransactionDB.GetTransaction(1).GetItems[0].Item1.GetName, "Test");
+
+        }
+    }
+}
