e3e168ef0af0b1200d1d6251580f0bc3fa534aab
Krishane Riegodedios <krishane@WIN2.CS.KSU.EDU>
2018-05-06 18:12:52 -0500

    Product Testing made

---
+++ PA-4/Architecture/PA4Tests/ProductModelTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA_4_GUI;
+
+namespace PA4Tests
+{
+    [TestClass]
+    public class ProductModelTest
+    {
+        [TestMethod]
+        public void TestProductMake()
+        {
+            Product p = new Product("Test", 4, 8.99);
+            Product c = new Product("Test", 4, 8.99);
+            Assert.AreEqual(p.GetName, c.GetName);
+        }
+
+        [TestMethod]
+        public void TestGetName()
+        {
+            Product p = new Product("Test", 4, 8.99);
+            Assert.AreEqual(p.GetName, "Test");
+        }
+
+        [TestMethod]
+        public void TestGetPrice()
+        {
+            Product p = new Product("Test", 4, 8.99);
+            Assert.AreEqual(p.GetPrice, 8.99);
+        }
+
+        [TestMethod]
+        public void TestGetID()
+        {
+            Product p = new Product("Test", 4, 8.99);
+            Assert.AreEqual(p.ID, 4);
+        }
+
+       
+
+
+
+        //Assert.AreEqual(p, new Product("Test", 4, 8.99));
+    }
+}
