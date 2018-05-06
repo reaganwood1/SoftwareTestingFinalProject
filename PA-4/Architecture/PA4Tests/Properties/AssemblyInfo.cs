1019ecd5dc2f2ca9b01820a115930806ea705188
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2018-05-06 15:36:14 -0500

    Added Unit Testing Suite

---
+++ PA-4/Architecture/PA4Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PA4Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kansas State University")]
+[assembly: AssemblyProduct("PA4Tests")]
+[assembly: AssemblyCopyright("Copyright © Kansas State University 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97d33199-f33f-4efd-ad53-489b20638622")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
