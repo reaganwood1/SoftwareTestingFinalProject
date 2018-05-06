8bd99f4c194e085f6bd8753ce6bb4a2e14f28582
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2018-05-06 15:58:48 -0500

    Added Unit Tests for ProductDB

---
+++ PA-4/Architecture/PA4Tests/ProductDBTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA_4_GUI;
+
+namespace PA4Tests
+{
+    [TestClass]
+    public class ProductDBTests
+    {
+        [TestMethod]
+        public void GetProductByID()
+        {
+            List<Product> products = ProductDB.GetProductList();
+            int numberOfProducts = products.Count;
+            Random randomGenerator = new Random();
+            int productIntToSelect = randomGenerator.Next(0, numberOfProducts - 1);
+            Product productToSelect = products[productIntToSelect];
+            Product returnedProduct = ProductDB.GetProduct(productIntToSelect);
+            Assert.Equals(returnedProduct, productToSelect);
+        }
+
+        [TestMethod]
+        public void GetProductListCheckNotNull()
+        {
+            List<Product> products = ProductDB.GetProductList();
+            Assert.IsNotNull(products);
+        }
+    }
+}
