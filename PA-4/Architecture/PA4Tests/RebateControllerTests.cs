33c54488ca723b0dff26eeaafc0c99a4b765ff92
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2018-05-06 20:17:19 -0500

    Added transaction controller tests

--- PA-4/Architecture/PA4Tests/RebateControllerTests.cs
+++ PA-4/Architecture/PA4Tests/RebateControllerTests.cs
@@ -33,7 +33,6 @@ namespace PA4Tests
             int itemQuantity = 1;
             double itemCost = 2.29;
             double rebatePercent = .05;
-            double totalRebate = itemQuantity * itemCost * rebatePercent;
             Product p1 = new Product("Diapers", itemID, itemCost);
             Tuple<Product, int> tuple1 = new Tuple<Product, int>(p1, itemQuantity);
             List<Tuple<Product, int>> items = new List<Tuple<Product, int>>();
@@ -54,5 +53,75 @@ namespace PA4Tests
             controller.RegisterRC(ob);
             controller.update("", State.Rebate);
         }
+
+        /// <summary>
+        /// Start of Transaction Controller Tests
+        /// </summary>
+        /// 
+        [TestMethod]
+        public void TestCreateTransaction()
+        {
+            //Create observer
+            Observer ob = new Observer((string s, State status) => { Assert.AreEqual("Transaction generated successfully!", s); Assert.AreEqual(status, State.Transaction); });
+
+            //add transaction through the transaction controller
+            TransactionController controller = new TransactionController();
+            controller.RegisterR(ob);
+            Tuple<int, int> productIDTransactionTyple = new Tuple<int, int>(1, 1);
+            List<Tuple<int, int>> tuples = new List<Tuple<int, int>>
+            {
+                productIDTransactionTyple
+            };
+            controller.CreateTransaction(tuples);
+        }
+
+        [TestMethod]
+        public void TestRegisterObserverTransactionController()
+        {
+            //Create observer
+            Observer ob = new Observer((string s, State status) => { Assert.AreEqual("", s); Assert.AreEqual(status, State.Transaction); });
+
+            //add transaction through the transaction controller
+            TransactionController controller = new TransactionController();
+            controller.RegisterR(ob);
+            controller.update("", State.Transaction);
+        }
+
+        [TestMethod]
+        public void TestCreateTransactionInvalidItemID()
+        {
+            //Create observer
+            Observer ob = new Observer((string s, State status) => { Assert.AreEqual("Invalid item ID ", s); Assert.AreEqual(status, State.Transaction); });
+
+            //add transaction through the transaction controller
+            TransactionController controller = new TransactionController();
+            controller.RegisterR(ob);
+            Tuple<int, int> productIDTransactionTyple = new Tuple<int, int>(1, 0);
+            List<Tuple<int, int>> tuples = new List<Tuple<int, int>>
+            {
+                productIDTransactionTyple
+            };
+            controller.CreateTransaction(tuples);
+        }
+
+        [TestMethod]
+        public void TestCreateTransactionInvalidItemQuantity()
+        {
+            //Create observer
+            Observer ob = new Observer((string s, State status) => { Assert.AreEqual("Invalid item quantity ", s); Assert.AreEqual(status, State.Transaction); });
+
+            //add transaction through the transaction controller
+            TransactionController controller = new TransactionController();
+            controller.RegisterR(ob);
+            Tuple<int, int> productIDTransactionTyple = new Tuple<int, int>(0, 0);
+            List<Tuple<int, int>> tuples = new List<Tuple<int, int>>
+            {
+                productIDTransactionTyple
+            };
+            controller.CreateTransaction(tuples);
+        }
+
+
+        /* NOTE: THIS Begins the return controller testing. There seems to be a problem with adding test modules to my version of visual studio. Basically the error is that tests will run in this module but not in any new module I create */
     }
 }
